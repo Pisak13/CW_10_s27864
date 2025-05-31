7a345e4d940cc5c0c853845cc99a9b26b7074e69
Damian Pisanski <s27864@pjwstk.edu.pl>
2025-05-31 14:51:49 +0200

    final commit v2

--- CW_10_s27864/Data/ApbdContext.cs
+++ CW_10_s27864/Data/ApbdContext.cs
@@ -26,7 +26,7 @@ public partial class ApbdContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=localhost, 1433; User=SA; Password=Damian1317; Initial Catalog=APBD; Integrated Security=False;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False");
+        => optionsBuilder.UseSqlServer("Data Source=localhost, 1433; User=SA; Password=********; Initial Catalog=APBD; Integrated Security=False;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
